db4d9cd82999cdeb7b14073169ef1249331f0154
Nathaniel Watson <nathankw@stanford.edu>
2016-10-03 18:30:15 -0700

    Added documentation to invite_user_to_projects.py, created accept_pending_transfers.py

--- accept_pending_transfers.py
+++ accept_pending_transfers.py
@@ -20,18 +20,16 @@ import dxpy
 
 DX_LOGIN_CONF = os.getenv("DX_LOGIN_CONF") #exported by the env module gbsc/dnanexus_seqresults
 
-description = "Invites a DNAnexus user to be a member of all projects billed to a the specified org, with the specified access level."
+description = "Given all pending DNAnexus project transfers for the specified user, allows the user to accept transfers under a specified billing account, but not just any project. Only projects that have the specified queue (set in the queue property of the project) will be transferred to the user."
 parser = ArgumentParser(description=description)
-parser.add_argument('-u',"--user-name",required=True,help="The DNAnexus login user name to invite to projects owned by the org specified by --org. An API token must have already been generated for this user and that token must have been added to the DNAnexus login configuration file located at {DX_LOGIN_CONF}.".format(DX_LOGIN_CONF=DX_LOGIN_CONF))
+parser.add_argument('-u',"--user-name",required=True,help="The DNAnexus login user name that is to receive pending transfers. An API token must have already been generated for this user and that token must have been added to the DNAnexus login configuration file located at {DX_LOGIN_CONF}.".format(DX_LOGIN_CONF=DX_LOGIN_CONF))
 parser.add_argument('-l','--access-level',required=True,choices=["VIEW","UPLOAD","CONTRIBUTE","ADMINISTER"],help="Permissions level the new member should have on shared projects.")
-parser.add_argument('-o',"--org",required=True,help="The name of the DNAnexus organization that the projects belong to.")
-parser.add_argument("--created-after",required=True,help="Date (e.g. 2012-01-01) or integer timestamp after which the project was created (negative number means in the past, or use suffix s, m, h, d, w, M, y)")
+parser.add_argument('-o',"--org",required=True,help="The name of the DNAnexus organization to bill transferred project to.")
 
 args = parser.parse_args()
 org = args.org
 user = args.user_name
 level = args.access_level
-created_after = args.created_after
 
 #LOG into DNAnexus (The environment module gbsc/dnanexus_seqresults should also be loaded in order to log into DNAnexus)
 subprocess.check_call("log_into_dnanexus.sh -u {du}".format(du=user),shell=True)
@@ -52,13 +50,10 @@ chandler.setFormatter(formatter)
 logger.addHandler(fhandler)
 logger.addHandler(chandler)
 
-#get projects since created_after date
-gen = dxpy.org_find_projects(org_id=org,created_after=created_after)
-#gen is a generator of dicts of the form 
-# {u'level': u'NONE', u'id': u'project-ByQ8kj8028GJ182PZx95Z3G2', u'public': False}
+#get pending transfers
+pending_tansfers = dxpy.api.user_describe(object_id=internal_dx_username,input_params={"pendingTransfers": True})["pendingTransfers"]
 
-#invite user to projects
-for i in gen:
+for i in pendingTransfers:
 	current_level = i["level"]
 	if current_level != level:
 		project = dxpy.DXProject(i["id"])
