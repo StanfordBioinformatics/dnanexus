38ad7d00f22e4ce06c00d30b2f6ffe08c49e09a8
Nathaniel Watson <nathankw@stanford.edu>
2018-02-15 20:40:49 -0800

    Added sphinx documentation

---
+++ docs/_build/html/_sources/scripts/invite_user_to_projects.rst.txt
@@ -0,0 +1,7 @@
+invite\_user\_to\_projects
+==========================
+
+.. argparse::
+   :module: gbsc_dnanexus.scripts.invite_user_to_projects
+   :func: get_parser
+   :prog: invite_user_to_projects.py
---
+++ setup.py
@@ -0,0 +1,21 @@
+
+# For some usefule documentation, see
+# https://docs.python.org/2/distutils/setupscript.html.
+# This page is useful for dependencies: 
+# http://python-packaging.readthedocs.io/en/latest/dependencies.html.
+
+from distutils.core import setup
+
+setup(
+  name = "gbsc_dnanexus",
+  version = "0.1.0",
+  description = "DNAnexus utilities",
+  author = "Nathaniel Watson",
+  author_email = "nathankw@stanford.edu",
+  url = "https://github.com/StanfordBioinformatics/gbsc_dnanexus",
+  packages = ["gbsc_dnanexus"],
+  install_requires = [
+    "dxyp",
+  ]
+  scripts = ["gbsc_utils/scripts/invite_user_to_projects.py"]
+)
